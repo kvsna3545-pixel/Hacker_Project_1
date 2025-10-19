72f485069444a0a2f39cc6cbff67ef7be4561e8e
Kvsna3545 <kvsna3545@gmail.com>
2025-10-19 13:09:01 +0700

    Update Project_Hacker_1.0.lua

--- Project_Hacker_1.0.lua
+++ Project_Hacker_1.0.lua
@@ -115,7 +115,7 @@ Scr.BackgroundColor3 = Color3.new(1, 1, 1)
 Scr.BorderColor3 = Color3.new(0, 0, 0)
 Scr.BorderSizePixel = 0
 Scr.Parent = Ui
-Scr.ScrollBarThickness = 1
+Scr.ScrollBarThickness = -1
 Scr.Visible = false
 
 Scr.BackgroundTransparency = 2
